2a58bf7c2afcc5cbbaa8bf49011f0c5c34480394
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-27 15:01:08 +0700

    commit 7 input no pewarisan dengan tambahan final setelah baseClass dan virtual void perkenalan

---
+++ nopewarisan.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari base Class";
+    }
+};
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya function dari derived Class";
+    }
+};
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
