c36d4fc0cecb62157f2a507d6e146c140996f26a
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-27 14:35:55 +0700

    commit 1 input class seseorang dengan public dan void pesan yg berisi cout "pesan dari seseorang"

---
+++ polymorphism.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+    // virtual void pesan() = O
+    void pesan(){
+        cout << "Pesan dari seseorang" << endl;
+    }
+    }
